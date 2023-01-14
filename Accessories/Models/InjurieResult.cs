e407a91b9da3e199c389316e5a3e263313707197
Lukáš Moravec <Lukasl32@hotmail.com>
2023-01-14 23:48:46 +0100

    Přidání InjurieResult Controller

---
+++ Accessories/Models/InjurieResult.cs
@@ -0,0 +1,13 @@
+﻿namespace Accessories.Models;
+
+public class InjurieResult
+{
+    public long Id { get; set; }
+    public long TeamId { get; set; }
+    public long InjurieId { get; set; }
+    public long RefereeId { get; set; }
+    public string? RefereeSignature { get; set; }
+    public string? LeaderSignature { get; set; }
+    public DateTime Signed { get; set; }
+    public DateTime Creted { get; set; }
+}
