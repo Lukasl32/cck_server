82b1c5c2dc6a250a51b33763388b1f63d6a283df
Lukáš Moravec <Lukasl32@hotmail.com>
2023-03-12 18:47:56 +0100

    Přidání endpointu pro výsledky ze stanovišť

---
+++ Accessories/Models/GeneralResult.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Accessories.Models;
+
+public class GeneralResult
+{
+    public long InjurieId { get; set; }
+    public long TeamId { get; set; }
+    public long RefereeId { get; set; }
+    public Signature Signature { get; set; }
+    [JsonPropertyName("tasks")]
+    public ResultTask[] ResultTasks { get; set; }
+}
+
+public class Signature
+{
+    public string Referee { get; set; }
+    public string Leader { get; set; }
+    public DateTime Signed { get; set; }
+}
+
+public class ResultTask
+{
+    public long Id { get; set; }
+    public int DeductedPoints { get; set; }
+}
+
+/*
+ {
+    "injurieId": 1,
+    "teamId": 1,
+    "refereeId": 1,
+    "signature": {
+        "referee": "564f6ba165c656d",
+        "leader": "12c569ad56c96e485b2",
+        "signed": "2023-4-21T15:13:55"
+    },
+    "tasks": [
+       {
+           "id": 1,
+           "deductedPoints": 20
+       },
+       {
+           "id": 2,
+           "deductedPoints": 0
+       }
+    ]
+}
+*/
---
+++ Api/Controllers/ResultController.cs
@@ -0,0 +1,121 @@
+﻿using System.Data.Common;
+using System.Text.Json;
+
+using Accessories.Enums;
+using Accessories.Extensions;
+using Accessories.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+using MySqlConnector;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Api.Controllers
+{
+    [Route("api/results")]
+    [ApiController]
+    public class ResultController : ControllerBase
+    {
+        //[HttpGet("{id}")]
+        //public async Task<IActionResult> Get(int id)
+        //{
+        //    Security.Authorize(HttpContext);
+        //    using MySqlConnection connection = new(Config.ConnString);
+        //    await connection.OpenAsync();
+
+
+        //    string sql = $";";
+        //    using MySqlCommand command = new(sql, connection);
+        //    using MySqlDataReader reader = await command.ExecuteReaderAsync();
+        //    if (await reader.ReadAsync())
+        //    {
+                
+        //    }
+        //    else
+        //        return NotFound();
+        //}
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            Security.Authorize(HttpContext);
+
+            var body = HttpContext.Request.Form;
+            
+            //----------------------------------------------------------------------------------------------
+            List<ResultTask> resultTasks = new();
+            for (int i = 0; i < 10; i++)
+            {
+                resultTasks.Add(new()
+                {
+                    Id = i,
+                    DeductedPoints = new Random().Next(-200, 200)
+                });
+            }
+
+            GeneralResult result = new()
+            {
+                InjurieId = 5,
+                TeamId = 6,
+                RefereeId = 7,
+                Signature = new()
+                {
+                    Referee = Security.GenerateHash(128),
+                    Leader = Security.GenerateHash(128),
+                    Signed = DateTime.Now
+                },
+                ResultTasks = resultTasks.ToArray()
+            };
+            string json = JsonSerializer.Serialize(result);
+            //----------------------------------------------
+            //GeneralResult data = JsonSerializer.Deserialize<GeneralResult>(json, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true})!;
+            //----------------------------------------------------------------------------------------------
+
+            GeneralResult data = JsonSerializer.Deserialize<GeneralResult>(body["data"]!, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true})!;
+            
+            using (MySqlConnection connection = new(Config.ConnString))
+            {
+                await connection.OpenAsync();
+
+                long resultInjurieId;
+
+                string sql = "INSERT INTO `result_injuries`(`team_id`, `injurie_id`, `referee_id`, `referee_signature`, `leader_signature`, `signed`) " +
+                    $"VALUES ('{data.TeamId}','{data.InjurieId}','{data.RefereeId}','{data.Signature.Referee}','{data.Signature.Leader}','{data.Signature.Signed:yyyy-MM-dd HH:mm:ss}');" +
+                    $"SELECT LAST_INSERT_ID();";
+                using (MySqlCommand command = new(sql, connection))
+                {
+                    try
+                    {
+                        resultInjurieId = Convert.ToInt64(await command.ExecuteScalarAsync());
+                    }
+                    catch (DbException ex)
+                    {
+                        //DB error code: 1062 - duplicated entry
+                        return Conflict();
+                    }
+                }
+
+                foreach (var task in data.ResultTasks)
+                {
+                    sql = "INSERT INTO `result_tasks`(`result_injurie_id`, `task_id`, `deducted_points`) " +
+                        $"VALUES ('{resultInjurieId}','{task.Id}','{task.DeductedPoints}');";
+                    using (MySqlCommand command = new(sql, connection))
+                    {
+                        try
+                        {
+                            await command.ExecuteNonQueryAsync();
+                        }
+                        catch (DbException ex)
+                        {
+                            //DB error code: 1062 - duplicated entry
+                            return Conflict();
+                        }
+                    }
+                }
+
+            }
+            return StatusCode(201);
+        }
+    }
+}
